bb773a1f85311adc5a3fad24321dc80422a81b95
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-06-26 03:05:18 -0700

    ecstatic issues but otherwise hopping

---
+++ server.js
@@ -0,0 +1,66 @@
+var http = require('http')
+,   ecstatic = require('ecstatic')
+,   answer = require('answerver')
+,   ls = require('ls-stream')
+,   path = require('path')
+,   fs = require('fs')
+,   openSesame = require('sesame-stream')
+;
+
+module.exports = function(dir, args){
+   
+  var home = dir.slice(dir.lastIndexOf('/'))
+  
+  var subdirs = {}
+  ls(dir).on('data', function(data){
+    if(data.stat.isDirectory()){
+      var duh = data.path.slice(data.path.lastIndexOf('/'))
+      if(duh == '/lib' || duh == '/node_modules' || duh == '/cmd' || duh == '/public') data.ignore()
+      else{
+        subdirs[duh] = data.stat
+        var ops = {
+          root: data.path + '/public',
+          //root: dir + duh + '/public',
+          autoIndex: true
+        }
+        subdirs[duh].static = ecstatic(ops)
+        data.ignore() // we only want the top level 
+      }
+    }
+  }).on('end', function(){console.log(subdirs)})
+
+
+  var server = http.createServer(function(req, res){
+      
+      var host = req.headers.host;
+      console.log(host, req.url)      
+      if(/^www/.test(host)){
+        res.writeHead(307, {'Location': 'http://' + host.replace(/^www\./, '') + req.url})
+        res.end()
+        return
+      }
+
+      var subdomain = host.split('.')[0]
+
+      console.log(subdomain)
+
+      subdirs['/' + subdomain].static(req, res)
+    
+      function next(){
+        res.writeHead(200, {'content-type' : 'text/html'})
+        fs.createReadStream(dir + home + '/index.html').pipe(res)
+      }
+
+  });
+
+  server.on('upgrade', openSesame)
+
+  answer(server, 11001)
+
+  server.on('error', function(e){
+  });
+
+  return server
+
+}
+
